23fed6591e8b7966f11e70b591847088874b2d49
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-11-28 20:39:25 +0100

    Tiempos de vida

---
+++ src/15_lifetimes/src/main.rs
@@ -0,0 +1,52 @@
+fn lifetime_1() {
+    let x = 5;
+
+    let r = &x;
+
+    println!("r: {}", r);
+}
+
+fn lifetime_2() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+fn lifetime_3() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+}
+
+fn lifetime_4() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+    //println!("The longest string is {}", result);   ERROR: string2 does not live long enough
+}
+
+fn main() {
+    lifetime_1();
+    lifetime_2();
+    lifetime_3();
+    lifetime_4();
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
