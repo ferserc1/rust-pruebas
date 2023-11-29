d0abd11523124dad16d3ba76c7c3479dee19585f
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-11-29 17:08:32 +0100

    Add ImportantExcerpt struct and lifetime_5 function

--- src/15_lifetimes/src/main.rs
+++ src/15_lifetimes/src/main.rs
@@ -35,11 +35,29 @@ fn lifetime_4() {
     //println!("The longest string is {}", result);   ERROR: string2 does not live long enough
 }
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+fn lifetime_5() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+    println!("Important Excerpt is '{}'", i.part);
+}
+
 fn main() {
     lifetime_1();
     lifetime_2();
     lifetime_3();
     lifetime_4();
+    lifetime_5();
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
